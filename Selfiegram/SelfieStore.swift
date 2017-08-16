7ecc1e74e3d92a558496f6af86b8b6d9e6c3ee05
McJones <tim@lonely.coffee>
2017-08-16 14:16:51 +1000

    selfie store method stubs

--- Selfiegram/SelfieStore.swift
+++ Selfiegram/SelfieStore.swift
@@ -66,6 +66,70 @@ enum SelfieStoreError : Error
 // BEGIN selfie_store_class
 final class SelfieStore
 {
+    // BEGIN shared_property
+    static let shared = SelfieStore()
+    // END shared_property
+    
+    // BEGIN method_stubs
+    /// Gets an image by ID. Will be cached in memory for future lookups.
+    /// - parameter id: the id of the selfie who's image you are after
+    /// - returns: the image for that selfie or nil if it doesn't exist
+    func getImage(id:UUID) -> UIImage?
+    {
+        return nil
+    }
+    
+    /// Saves an image to disk.
+    /// - parameter id: the id of the selfie you want this image associated with
+    /// - parameter image: the image you want saved
+    /// - Throws: `SelfieStoreObject` if it fails to save to disk
+    func setImage(id:UUID, image : UIImage?) throws
+    {
+        throw SelfieStoreError.cannotSaveImage(image)
+    }
+    
+    /// Returns a list of Selfie objects loaded from disk.
+    /// - returns: an array of all selfies previously saved
+    /// - Throws: `SelfieStoreError` if it fails to load a selfie correctly from disk
+    func listSelfies() throws -> [Selfie]
+    {
+        return []
+    }
+    
+    /// Deletes a selfie, and its corresponding image, from disk.
+    /// This function simply takes the ID from the Selfie you pass in,
+    /// and gives it to the other version of the delete function.
+    /// - parameter selfie: the selfie you want deleted
+    /// - Throws: `SelfieStoreError` if it fails to delete the selfie from disk
+    func delete(selfie: Selfie) throws
+    {
+        throw SelfieStoreError.cannotSaveImage(nil)
+    }
+    
+    /// Deletes a selfie, and its corresponding image, from disk.
+    /// - parameter id: the id property of the Selfie you want deleted
+    /// - Throws: `SelfieStoreError` if it fails to delete the selfie from the disk
+    func delete(id: UUID) throws
+    {
+        throw SelfieStoreError.cannotSaveImage(nil)
+    }
+    
+    /// Attempts to load a selfie from disk.
+    /// - parameter id: the id property of the Selfie object you want loaded from disk
+    /// - returns: The selfie with the matching id, otherwise nil
+    func load(id: UUID) -> Selfie?
+    {
+        return nil
+    }
+    
+    /// Attempts to save a selfie to disk
+    /// - parameter selfie: the selfie to save to disk
+    /// - Throws: `SelfieStoreError` if it fails to write the data
+    func save(selfie: Selfie) throws
+    {
+        throw SelfieStoreError.cannotSaveImage(nil)
+    }
+    // END method_stubs
 }
 // END selfie_store_class
 
