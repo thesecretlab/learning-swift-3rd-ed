2306eec7609d5538e5b6ae5242b0ac71a6cbcf18
McJones <tim@lonely.coffee>
2017-08-16 12:58:12 +1000

    empty Selfie class

---
+++ Selfiegram/SelfieStore.swift
@@ -0,0 +1,18 @@
+//
+//  SelfieStore.swift
+//  Selfiegram
+//
+//  Created by Tim Nugent on 14/8/17.
+//  Copyright © 2017 Lonely Coffee. All rights reserved.
+//
+
+import Foundation
+// BEGIN model_imports
+import UIKit.UIImage
+// END model_imports
+
+// BEGIN model_selfie_class_initial
+class Selfie : Codable
+{
+}
+// END model_selfie_class_initial
