f20907d8aca348b190db8fc5d6a048107d883785
McJones <tim@lonely.coffee>
2017-09-12 14:15:12 +1000

    settings view controller now saves and loads the location preference correctly

--- Selfiegram/SettingsTableViewController.swift
+++ Selfiegram/SettingsTableViewController.swift
@@ -8,16 +8,32 @@
 
 import UIKit
 
+// BEGIN settings_enum
+enum SettingsKey : String
+{
+    case saveLocation
+}
+// END settings_enum
+
 class SettingsTableViewController: UITableViewController {
 
     @IBOutlet weak var locationSwitch: UISwitch!
     
+    // BEGIN settings_toggle_method
     @IBAction func locationSwitchToggled(_ sender: Any)
     {
+        // Update the setting in UserDefaults.
+        UserDefaults.standard.set(locationSwitch.isOn, forKey: SettingsKey.saveLocation.rawValue)
     }
+    // END settings_toggle_method
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // BEGIN settings_viewDidLoad
+        // Make sure that the location switch is set correctly.
+        locationSwitch.isOn = UserDefaults.standard.bool(forKey: SettingsKey.saveLocation.rawValue)
+        // END settings_viewDidLoad
     }
 
     override func didReceiveMemoryWarning() {
