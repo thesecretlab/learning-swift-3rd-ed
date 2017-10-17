75c4b15c92693bf34d5e755ff7edf617ccec7c2a
McJones <tim@lonely.coffee>
2017-10-17 13:37:49 +1100

    UI for the editing view

---
+++ Selfiegram/EditingViewController.swift
@@ -0,0 +1,37 @@
+//
+//  EditingViewController.swift
+//  Selfiegram
+//
+//  Created by Tim Nugent on 17/10/17.
+//  Copyright © 2017 Lonely Coffee. All rights reserved.
+//
+
+import UIKit
+
+class EditingViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var optionsStackView: UIStackView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
