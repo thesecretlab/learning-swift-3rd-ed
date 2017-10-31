07af0846b4d2183d59d1e883b893c364f5907d6c
McJones <tim@lonely.coffee>
2017-10-31 15:36:01 +1100

    added UI tests

--- SelfiegramUITests/SelfiegramUITests.swift
+++ SelfiegramUITests/SelfiegramUITests.swift
@@ -8,6 +8,8 @@
 
 import XCTest
 
+//@testable import Selfiegram
+
 class SelfiegramUITests: XCTestCase {
         
     override func setUp() {
@@ -28,9 +30,64 @@ class SelfiegramUITests: XCTestCase {
         super.tearDown()
     }
     
+    // BEGIN UI_test_example
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let app = XCUIApplication()
+        let currentSelfieCount = app.tables.element(boundBy: 0).cells.count
+        
+        app.terminate()
+        app.launch()
+        
+        let tables = app.tables.element(boundBy: 0)
+        XCTAssertEqual(currentSelfieCount, tables.cells.count)
     }
+    // END UI_test_example
     
+    // BEGIN ui_test_existence
+    func testExistence() {
+        // BEGIN ui_test_existence_code
+        let app = XCUIApplication()
+        app.tables.element(boundBy: 0).cells.element(boundBy: 0).tap()
+        
+        let mapView = app.maps.firstMatch
+        // END ui_test_existence_code
+        
+        // BEGIN ui_test_existence_assert
+        XCTAssert(mapView.exists)
+        XCTAssert(mapView.isHittable)
+        // END ui_test_existence_assert
+    }
+    // END ui_test_existence
+    
+    // BEGIN ui_test_photo
+        func testPhotos () {
+        // BEGIN ui_test_photo_alert_handler
+        addUIInterruptionMonitor(withDescription: "Camera Permission Dialog")
+        { (alert) -> Bool in
+            alert.buttons["OK"].tap()
+            return true
+        }
+        // END ui_test_photo_alert_handler
+        
+        // BEGIN ui_test_photo_record
+        let app = XCUIApplication()
+        
+        // BEGIN ui_test_photo_setup
+        let currentSelfieCount = app.tables.element(boundBy: 0).cells.count
+        // END ui_test_photo_setup
+        
+        app.navigationBars["Selfies"].buttons["Add"].tap()
+            
+        app.children(matching: .window).element(boundBy: 0)
+           .children(matching: .other).element.tap()
+            
+        app.navigationBars["Edit"].buttons["Done"].tap()
+        // END ui_test_photo_record
+        
+        // BEGIN ui_test_photo_test
+        let tables = app.tables.element(boundBy: 0)
+        XCTAssertEqual(currentSelfieCount + 1, tables.cells.count)
+        // END ui_test_photo_test
+    }
+    // END ui_test_photo
 }
